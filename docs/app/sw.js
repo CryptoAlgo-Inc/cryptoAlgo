5065fd7db609e824a2d338a975921302d15c7ef5
cryptoAlgorithm <cryptoAlgorithm@users.noreply.github.com>
2020-04-24 16:25:13 +0800

    Add web app file

---
+++ docs/app/sw.js
@@ -0,0 +1,38 @@
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       'generic.html',
+       'downloads.html',
+       'contact.html',
+       'index.html',
+       'assets/css/main.css',
+       'assets/js/jquery.min.js',
+       'assets/js/jquery.scrollex.min.js',
+       'assets/js/jquery.scrolly.min.js',
+       'assets/js/main.js',
+       'assets/js/skel.min.js',
+       'assets/js/util.js',
+       'assets/fonts/FontAwesome.otf'
+       'assets/fonts/fontawesome-webfont.eot',
+       'assets/fonts/fontawesome-webfont.svg',
+       'assets/fonts/fontawesome-webfont.ttf',
+       'assets/fonts/fontawesome-webfont.woff',
+       'assets/fonts/fontawesome-webfont.woff2'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
