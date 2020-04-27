e4b9da7fdfc96bc6add0bb0724dcd83f790f03dd
cryptoAlgorithm <cryptoAlgorithm@users.noreply.github.com>
2020-04-27 21:02:25 +0800

    Update main.js

--- Sources-GUI/assets/js/main.js
+++ Sources-GUI/assets/js/main.js
@@ -17,7 +17,6 @@
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
-
 			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
