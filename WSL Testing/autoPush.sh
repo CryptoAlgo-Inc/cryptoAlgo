6f0720f4da72d7b52a379783c916b2448ceb5da9
CryptoAlgorithm <cryptoalgro@gmail.com>
2020-05-10 22:43:16 +0800

    Added push script and updated it

--- WSL Testing/autoPush.sh
+++ WSL Testing/autoPush.sh
@@ -1,8 +1,13 @@
 echo "Adding any new files to git"
-if git add . && git commit; then
+git add .
+if git commit; then
     echo "Success"
     echo "Pushing changes to GitHub"
-    git push
+    if git push; then
+        echo "Successfully completed"
+    else
+        echo "Could not push files to Git. Please check your username and password"
+    fi
 else
     echo "No new files to add or commit aborted"
 fi
