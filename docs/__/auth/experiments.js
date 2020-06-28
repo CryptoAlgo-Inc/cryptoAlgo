18022f345bb86070d85d9a539d16d955c7c8673e
CryptoAlgo Inc <support@cryptoalgo.cf>
2020-06-28 17:55:32 +0800

    Revert "Changed email verification page"
    This reverts commit ec340dcebe5c81608b865e9dae00242d56a984b9.

--- docs/__/auth/experiments.js
+++
@@ -1,6 +0,0 @@
-(function(){/*
-
- Copyright The Closure Library Authors.
- SPDX-License-Identifier: Apache-2.0
-*/
-window.EXPERIMENTS={DISPLAY_CONTINUE_BUTTON_IF_NOT_REDIRECT:{id:1000003,rollout:1,defaultValue:!0,expiration:new Date("April 1, 2020"),stagingRollout:1},CHECK_CONTINUE_URL_IS_AUTHORIZED:{id:1000004,rollout:1,defaultValue:!0,expiration:new Date("September 1, 2020"),stagingRollout:1}};}).call(this);
