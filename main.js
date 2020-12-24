1f2290862fcc3abeb122d10235bcc8b03b94f91b
CryptoAlgo-Dev <64193267+cryptoAlgorithm@users.noreply.github.com>
2020-12-24 17:01:29 +0800

    Add description in package.json

--- main.js
+++ main.js
@@ -65,14 +65,14 @@ app.whenReady().then(createWindow);
 // Manage macOS window behaviour
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        createWindow();
     }
-})
+});
